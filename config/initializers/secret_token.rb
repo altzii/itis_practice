2677e5396a7de7cd6d9510a639dbba46d7ce9003
Alexander Leontjev <sleontjev@yandex.ru>
2015-08-22 12:21:08 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Itis::Application.config.secret_key_base = 'cf190fd645c0293603d7823d193497a2fb4e13e4550f8d1e7c497439ded0c64514a48d3bb958c760bb66b726e6fe41ed9b0c2afdcba92463bc44ebf0f1fda611'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Itis::Application.config.session_store :cookie_store, key: '_itis_session'
